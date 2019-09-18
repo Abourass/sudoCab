68f3143eb3831b72be2c832191228acadb7a4857
Abourass <abourassa@assetval.com>
2019-09-18 08:46:00 -0600

    Adding some more de

--- German-setup.sh
+++ German-setup.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
-cp vocab-German.sh "$HOME/vocab-German.sh"
-cp .vocab-script.sh "$HOME/.vocab-script.sh"
+wget -O ~/.vocab-German.sh "https://raw.githubusercontent.com/Abourass/sudoCab/master/vocab-German.sh" --no-check-certificate
+wget -O ~/.vocab-script.sh "https://raw.githubusercontent.com/Abourass/sudoCab/master/.vocab-script.sh" --no-check-certificate
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	OSBASHRC=bashrc
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	OSBASHRC=bash_profile
 fi
-if ! grep -Fxq "$HOME/.vocab-German.sh" ~/.$OSBASHRC; then
-	echo $"\n$HOME/.vocab-German.sh" >> ~/.$OSBASHRC
+if ! grep -Fxq '~/.vocab-German.sh' ~/.$OSBASHRC; then
+	echo $'\n~/.vocab-German.sh' >> ~/.$OSBASHRC
 fi
 
 OSBASHRC="zshrc"
 if [[ -f ~/.$OSBASHRC ]]; then
-	if ! grep -Fxq "$HOME/.vocab-German" ~/.$OSBASHRC; then
-		echo $"\n$HOME/.vocab-German" >> ~/.$OSBASHRC
+	if ! grep -Fxq '~/.vocab-German' ~/.$OSBASHRC; then
+		echo $'\n~/.vocab-German' >> ~/.$OSBASHRC
 	fi
 fi
 
-echo $"chmod u+x $HOME/.vocab-German.sh"
-echo $"chmod u+x $HOME/.vocab-script.sh"
+echo $"chmod u+x ~/.vocab-German.sh"
+echo $"chmod u+x ~/.vocab-script.sh"
